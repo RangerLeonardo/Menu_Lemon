a340e8bd5c6534234492c1ee00dbfa99d930ea6c
RangerLeonardo <brandonleonardobarrera@gmail.com>
2025-01-18 01:25:17 -0600

    Menu Lemon

--- docs/build/scripts/scripts.js
+++ docs/build/scripts/scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var link = document.createElement("a");
-    link.href = "/docs/build/media/PDF/menu_lemon.pdf"; 
+    link.href = "../media/PDF/menu_lemon.pdf"; 
     link.download = "menu_lemon.pdf"; // Nombre del archivo a descargar
     document.body.appendChild(link);
     link.click();
